c8ca7f19b185d8468411d87ec8c08aedf7321fc6
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-02 13:56:56 +0000

    added post model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    votes = models.IntegerField()
+
+class Group(models.Model):
+    group_id = models.AutoField(primary_key=True)
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
--- kebbit/settings.py
+++ kebbit/settings.py
@@ -83,6 +83,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-harrisong5-finalproject-wzsc1w9pm4v.ws.codeinstitute-ide.net", 
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
