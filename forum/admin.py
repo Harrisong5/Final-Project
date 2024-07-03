ae1b7e19db89d7d4844248658cb27e1bcc3b0dad
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-03 12:10:35 +0000

    redefined post model to use community model

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Post, Community, JoinComm
+from .models import Community, JoinComm, Post
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Community)
 admin.site.register(JoinComm)
+admin.site.register(Post)
--- forum/models.py
+++ forum/models.py
@@ -3,22 +3,6 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
-class Post(models.Model):
-    post_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True)
-    content = models.TextField()
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="forum_posts"
-    )
-    timestamp = models.DateTimeField(auto_now_add=True)
-    votes = models.IntegerField(default=0)
-    status = models.IntegerField(choices=STATUS, default=0)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Post, self).save(*args, **kwargs)
 
 class Community(models.Model):
     commUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_communities")
@@ -40,10 +24,29 @@ class JoinComm(models.Model):
     def __str__(self):
         return self.JoinCommUser.username
 
-class Sub(models.Model):
-    sub_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200, unique=True)
-    desc = models.TextField()
+class Post(models.Model):
+    postID = models.AutoField(primary_key=True)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name="post_comm")
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    content = models.TextField()
+    imgLink = models.CharField(max_length=1500, null=True, blank=True)
+    youtubeLink = models.CharField(max_length=1500, null=True, blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="forum_posts"
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    votes = models.ManyToManyField(User, related_name="votes")
+    comments = models.IntegerField(null=True, blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
