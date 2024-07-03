5b1b458cffcfc340f9af595fc1e9ee8891b7410b
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-03 13:03:07 +0000

    views test for posts and comments

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Comment
 
 # Create your views here.
+posts = Post.objects.all().order_by('-date')
+comments = Comment.objects.all()
+
 def my_forum(request):
-    return HttpResponse("hunter bro")
+    return HttpResponse(comments)
+
     
