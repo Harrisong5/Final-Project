327ad71087cff22c133a89b31a034a573350a2ef
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-03 17:44:20 +0000

    base and index html added and css static

--- forum/views.py
+++ forum/views.py
@@ -7,5 +7,6 @@ posts = Post.objects.all().order_by('-date')
 comments = Comment.objects.all().order_by('-post')
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "forum/index.html"
+    paginate_by = 6
 
