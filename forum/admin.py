983d1b0531a872de1602a02c219efee9d197136d
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-03 10:58:33 +0000

    added community join model

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Community
+from .models import Post, Community, JoinComm
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Community)
+admin.site.register(JoinComm)
--- forum/models.py
+++ forum/models.py
@@ -28,11 +28,17 @@ class Community(models.Model):
     commImglink = models.CharField(max_length=500, null=True, blank=True)
     commDesc = models.TextField(max_length=1000, null=True, blank=True)
     commDate = models.DateTimeField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.commTitle
 
+class JoinComm(models.Model):
+    JoinCommUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_joined_comms")
+    JoinComm = models.ManyToManyField(Community, related_name="forum_comms")
+    Joined = models.CharField(max_length=100, null=True, blank=True, default='FALSE')
 
+    def __str__(self):
+        return self.JoinCommUser.username
 
 class Sub(models.Model):
     sub_id = models.AutoField(primary_key=True)
