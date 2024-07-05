fbd122b07096799e9c51caae3e8abfcb88cdaf74
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-05 14:05:40 +0000

    added register login and dashboard urls

--- forum/urls.py
+++ forum/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='forum'),
+    path('register', views.register, name='register'),
+    path('my_login', views.my_login, name='my_login'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Post, Comment
+from . forms import CreateUserForm
+from django.http import HttpResponse
 
 # Create your views here.
 posts = Post.objects.all().order_by('-date')
@@ -10,3 +12,28 @@ class PostList(generic.ListView):
     template_name = "forum/index.html"
     paginate_by = 6
 
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redierct('forum/index.html')
+
+    context = {'registerform':form}
+
+    return render (request, 'forum/register.html', context=context)
+
+def my_login(request):
+
+   return HttpResponse("login page")
+
+def dashboard(request):
+
+    return HttpResponse("dashboard")
