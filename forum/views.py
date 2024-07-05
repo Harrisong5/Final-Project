f2a21832e28e2f58c4e1e230a6ebec708a288332
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-05 14:32:11 +0000

    added login form and redirects to dashboard

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Post, Comment
-from . forms import CreateUserForm
+from . forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 
 # Create your views here.
@@ -24,7 +26,7 @@ def register(request):
 
             form.save()
 
-            return redirect('forum')
+            return redirect('my_login')
 
     context = {'registerform':form}
 
@@ -32,7 +34,28 @@ def register(request):
 
 def my_login(request):
 
-   return HttpResponse("login page")
+    form = LoginForm()
+
+    if request.method == "POST":
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect("dashboard")
+
+    context = {'loginform':form}
+
+    return render (request, 'forum/my_login.html', context=context)
 
 def dashboard(request):
 
