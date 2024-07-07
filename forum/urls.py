689959d493fdf64c389b3121349228b673e22a0e
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-07 20:23:16 +0000

    added home and login links to base html

--- forum/urls.py
+++ forum/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, reverse
 urlpatterns = [
     path('', views.PostList.as_view(), name='forum'),
     path('register', views.register, name='register'),
-    path('login', views.my_login, name='my_login'),
+    path('login', views.my_login, name='login'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('logout', views.logout, name='logout'),
     path('post/<int:pk>', views.post_detail, name='post_detail'),
