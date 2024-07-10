8ce57655074323da6b9fd98536027c2bee3869c9
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-07-10 12:39:26 +0000

    responsivenss fix

--- kebbit/settings.py
+++ kebbit/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-harrisong5-finalproject-14dwxlo3xn3.ws.codeinstitute-ide.net', 
 '.herokuapp.com']
